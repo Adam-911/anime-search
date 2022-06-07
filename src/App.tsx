ff1ef9e1bb3dd942ccd475398a58f163cb0caddc
Adam <agutov@magnumtech.kz>
2022-06-07 16:57:00 +0600

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <form>
+                <span>Список аниме</span>
+                <input type="text" placeholder='Введите название'/>
+            </form>
+        </div>
+    );
+}
+
+export default App;
